f633ab0623bd53fec8e24f5192d8f2ddbd8e8641
eat-alone <130672017+eat-alone@users.noreply.github.com>
2023-04-16 19:11:27 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        num1 = num1 * 1000;
+        answer = num1 / num2;
+        return answer;
+    }
+}
