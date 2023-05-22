7dc4e2eaa2f22ae2f8ffc055fc85cf0df7a65d32
eat-alone <130672017+eat-alone@users.noreply.github.com>
2023-05-22 23:59:47 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 1.26 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,8 @@
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numList) {
+        return (Arrays.stream(numList).reduce((acc, i) -> acc * i).getAsInt() < Math.pow(Arrays.stream(numList).sum(), 2)) ? 1 : 0;
+    }
+}
