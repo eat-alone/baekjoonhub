04b67da11c981a49b9d5442c650c421455d5409d
eat-alone <130672017+eat-alone@users.noreply.github.com>
2023-06-20 16:12:58 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "fail";
+        for(int i = 0; i < db.length; i++){
+            if(db[i][0].length() == id_pw[0].length() && db[i][0].equals(id_pw[0])){
+                if(db[i][1].length() == id_pw[1].length() && db[i][1].equals(id_pw[1])){
+                    answer = "login";
+                    return answer;
+                }else{
+                    answer = "wrong pw";
+                    return answer;
+                }
+            }
+        }
+        return answer;
+    }
+}
